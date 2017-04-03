1a1a7bdc7e336ec25ee2f25becf0b9573862cb4e
Zach-Attach <zachlaborde93@gmail.com>
2017-04-03 17:00:50 -0500

    got most of the basic code done

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,31 @@
 package sample;
 
-public class Controller {
+import javafx.beans.value.ChangeListener;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+public class Controller implements Initializable{
+
+    @FXML
+    TextField passwordField;
+    @FXML
+    ProgressBar difficultyBar;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        ChangeListener<String> changeListener = (observable, oldValue, newValue) -> {
+            System.out.println("You did something!\n");
+        };
+
+        passwordField.textProperty().addListener(changeListener);
+
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -9,7 +9,7 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("PassWard");
         primaryStage.setScene(new Scene(root, 300, 275));
@@ -20,4 +20,54 @@ public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+
+    int getSpaceDepth(String s) {
+
+        //TODO - FINISH THIS USING REGEXXXXX :P
+
+//        Space Depth (SD) - num = 10, alpha=26, symbol=33; sum of these
+//  Ex: 1a# = 10+26+33 = 69
+
+        return 0;
+    }
+
+    long getSearchSpace(int spaceDepth, int length) {
+
+        // Search Space Size = SD^n + SD^(n-1) + ... + SD^1
+
+        long rVal = 0;
+
+        for (; length > 0; length--) {
+            rVal += Math.pow(spaceDepth, length);
+        }
+
+        return rVal;
+    }
+
+    double[] getTimeEstimations(long searchSpace) {
+
+/*        TIME
+
+        Online Attack Scenario - 1,000 guesses/sec
+
+        Offline Fast Attack Scenario - 1 x 10^9 guesses/sec   (1 billion)
+
+        Massive Cracking Array Scenario - 1 x 10^12 guesses/sec (1 trillion)*/
+
+        return new double[]{
+                searchSpace / Math.pow(10, 9),
+                searchSpace / Math.pow(10, 9),
+                searchSpace / Math.pow(10, 12)
+        };
+
+    }
+
+    /*
+
+
+
+COLOR INDICATION (cutoff points)
+
+SUGGESTIONS (you should use a symbol or whatever)
+*/
 }
